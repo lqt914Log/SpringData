8eb0bdf11d63b3bbacd60b3a9aa1c022434c7228
liuqiteng <you@example.com>
2019-03-05 17:46:44 +0800

    提交发送邮件逻辑

---
+++ sendEmail/src/main/java/com/lqt/email/Application.java
@@ -0,0 +1,14 @@
+package com.neo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+		System.out.println("项目启动成功");
+	}
+}
---
+++ sendEmail/src/main/java/com/lqt/email/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.lqt.email.controller;
+
+/**
+ * TEST
+ *
+ * @author lqt77
+ * @create 2019-03-05-16:31
+ */
+public class TestController {
+}
